c5c92ff55c9a5bf42bcc94d24a8e644189326fac
Victor-Lis <victorlisbronzo1@gmail.com>
2026-06-22 17:16:44 -0300

    feat: add middleware proxy configuration for NextAuth path filtering

--- web/src/proxy.ts
+++ web/src/proxy.ts
@@ -1,8 +1,13 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
+import { NextResponse } from "next/server";
 
 // export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth).*)"],
 };
+
+export default function proxy(request: Request) {
+  return NextResponse.next();
+}
