b35264c9a3ff7bfe5138b022911cc5cca263b584
Mariana <marimsampaio@outlook.com>
2026-06-24 18:06:25 -0300

    fix: update user service implementation

--- api/src/services/user/user.service.ts
+++ api/src/services/user/user.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+const bcrypt = require('bcryptjs');
 import { IUserDAO } from '@/dao/user/user.dao.interface';
 import type { IUserService, AuthenticateResult } from './user.service.interface';
 import { NotFoundError } from '@/errors/not-found.error';
